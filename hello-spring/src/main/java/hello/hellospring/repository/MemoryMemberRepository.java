7b5d4b112f2272231f46dc84feb96a06d7772e51
yejiin <yj_song67@naver.com>
2021-04-13 21:41:14 +0900

    domain, repository, repository test

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId((++sequence));
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));  // null이 반환될 가능성이 있으면 Optional로 감싸서 반환하여 클라이언트에서 처리
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();  // findAny() 하나라도 찾으면 return
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
