28f851f2c67a95acc7f6d4cada667ccc50074255
yejiin <yj_song67@naver.com>
2021-04-20 21:03:35 +0900

    add new discount policy, AppConfig

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,37 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+/**
+ * AppConfig
+ * 애플리케이션의 전체 동작 방식을 구성
+ * 구현 객체를 생성하고, 연결하는 책임을 가지는 설정 클래스
+ * 구성 정보에서 역할과 구현을 명확하게 분리해야 함
+ */
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
