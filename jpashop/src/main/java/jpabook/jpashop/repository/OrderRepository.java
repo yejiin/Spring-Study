cbb4b86e90b370a81aadfbd17ef3786fa6c56b04
yejiin <yj_song67@naver.com>
2021-05-02 17:42:27 +0900

    add View, Controller

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -30,7 +30,7 @@ public class OrderRepository {
     public List<Order> findAllByString(OrderSearch orderSearch) {
 
         //language = JPQL
-        String jpql = "select i from Order o join o.member m";
+        String jpql = "select o from Order o join o.member m";
         boolean isFirstCondition = true;
 
         //주문 상태 검색
--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -53,9 +55,7 @@ public class OrderService {
     }
 
     /** 주문 검색 */
-/*
     public List<Order> findOrders(OrderSearch orderSearch) {
-        return orderRepository.findAll(orderSearch);
+        return orderRepository.findAllByString(orderSearch);
     }
-*/
 }
