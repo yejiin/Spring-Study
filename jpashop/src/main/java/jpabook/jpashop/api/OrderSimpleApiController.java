1c7591148a15e3ef588bb751acca310cedf89c10
yejiin <yj_song67@naver.com>
2021-05-04 15:06:04 +0900

    lazy loading, select optimization

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,110 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * xToOne (ManyToOne, OneToOne) 관계 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등룍, LAZY=null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore (양방향 연관관계 중 한 곳에 @JsonIgnore 를 해줘햐 한다.)
+     * @return
+     */
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();  //Lazy 강제 초기화
+            order.getDelivery().getAddress();  //Lazy 강제 초기화
+        }
+        return all;
+    }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO 로 변환 (fetch join 사용x)
+     * - 단점: 지연로딩으로 쿼리 N번 호출 (N + 1 문제 발생)
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        //ORDER 2개
+        //N + 1 -> 1(orders) + 회훤 N(2) + 배송 N(2) = 총 5번 실행 , EAGER로 바꿔도 해결 x(EAGER 사용 x, 최적화 안됨, query 예측 안됨)
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+
+//        return orderRepository.findAllByString(new OrderSearch()).stream()
+//                .map(SimpleOrderDto::new)
+//                .collect(Collectors.toList());
+    }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO 로 변환 (fetch join 사용o)
+     * - fetch join 으로 쿼리 1번 호출
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    /**
+     * V4. JPA 에서 DTO 로 바로 조회
+     * - 쿼리 1번 호출
+     * - select 절에서 원하느 데이터만 선택해서 조회
+     * - 네트워크 용량 최적화
+     * - repository 재사용성 떨어짐
+     */
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();  //LAZY 초기화
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();  //LAZY 초기화
+        }
+    }
+}
