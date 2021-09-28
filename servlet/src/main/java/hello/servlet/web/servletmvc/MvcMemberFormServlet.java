a62c0617b74d1bd043c5b05f002b721dddeee1f8
yejiin <yj_song67@naver.com>
2021-09-28 14:05:34 +0900

    Servlet, JSP, MVC 패턴

---
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,20 @@
+package hello.servlet.web.servletmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);// JSP 나 서블릿으로 요청을 넘기는 역할
+        dispatcher.forward(request, response);// forward : 다른 서블릿이나 jsp 로 이동. 서버 내부에서 다시 호출 발생
+    }
+}
