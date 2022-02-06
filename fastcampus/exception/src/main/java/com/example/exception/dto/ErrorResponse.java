98a56bbbc3ba5275863e4ff8033b425c7689c8d5
yejiin <yj_song67@naver.com>
2022-02-07 00:59:44 +0900

    Spring Boot Validation 활용

---
+++ fastcampus/exception/src/main/java/com/example/exception/dto/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.example.exception.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ErrorResponse {
+
+    String statusCode;
+    String requestUrl;
+    String code;
+    String message;
+    String resultCode;
+
+    List<Error> errorList;
+
+}
